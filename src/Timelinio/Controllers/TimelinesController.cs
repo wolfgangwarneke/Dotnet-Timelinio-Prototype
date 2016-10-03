429e562fdff8fa18359419f299e4ab9a55a147bf
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-10-03 11:30:11 -0700

    add create timeline with dropdown list for associated subject focus

--- src/Timelinio/Controllers/TimelinesController.cs
+++ src/Timelinio/Controllers/TimelinesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Timelinio.Models;
 using Timelinio.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -44,6 +45,34 @@ namespace Timelinio.Controllers
             }
 
             return View(student);
+        }
+
+        public IActionResult Create()
+        {
+            PopulateFocusesDropDownList();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Credits,FocusID,Title,Description,BeginDate,EndDate")] Timeline timeline)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(timeline);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            PopulateFocusesDropDownList(timeline.FocusID);
+            return View(timeline);
+        }
+
+        private void PopulateFocusesDropDownList(object selectedFocus = null)
+        {
+            var focusesQuery = from f in _context.Focuses
+                                   orderby f.Name
+                                   select f;
+            ViewBag.FocusID = new SelectList(focusesQuery, "FocusID", "Name", selectedFocus);
         }
     }
 }
