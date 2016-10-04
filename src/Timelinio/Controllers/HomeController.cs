b55642f8b852319611952da5a8970ed19af1827b
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2016-10-04 06:08:48 -0700

    add a bit more styling and add basic content on about and contact pages

--- src/Timelinio/Controllers/HomeController.cs
+++ src/Timelinio/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Timelinio.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Want to know more about Timelinio?";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Feedback? Get ahold of us.";
 
             return View();
         }
